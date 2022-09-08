1de86ef5a4b9f92ccd96f22872e27316b9cd709f
¨Paulo <paulohumano@gmail.com>
2022-09-08 15:15:55 -0300

    corregi el ejercicio arte ascii

---
+++ ejercicio_1.py
@@ -0,0 +1,9 @@
+# crear variables
+a = float(2.4)
+b = float(-3.21)
+c = float(47.8)
+# dar formato a print
+print("a \t", "b \t", "c")
+print(a, end="\t")
+print(b, end="\t")
+print(c)
---
+++ ejercicio_2.py
@@ -0,0 +1,7 @@
+# pip install image_to_Ascii
+
+from image_to_ascii import ImageToAscii
+
+ImageToAscii(imagePath="quijote.jpg",outputFile="image.txt")
+open('image.txt')
+print('image.txt')
